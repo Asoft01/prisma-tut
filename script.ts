9417590c7eff65313f2f50e94a629ff342e08d0f
Adeleke Hammed <lekhad19@gmail.com>
2024-03-19 15:22:13 +0100

    first commit

---
+++ script.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client'; 
+const prisma = new PrismaClient()
+
+async function main() {
+    // ... you will write your prisma client queries here 
+}
+
+main()
+    .catch(e => {
+        console.log(e.message)
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })

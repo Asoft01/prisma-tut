250bf4fbc57d90689934273435e6edb185420c2f
Adeleke Hammed <lekhad19@gmail.com>
2024-03-20 13:34:04 +0100

    schema structuring

--- script.ts
+++ script.ts
@@ -3,7 +3,8 @@ const prisma = new PrismaClient()
 
 async function main() {
     // ... you will write your prisma client queries here 
-    const user = await prisma.user.create({ data: { name : 'Kyle'}});
+    // const user = await prisma.user.create({ data: { name : 'Kyle'}});
+    const user = await prisma.user.findMany();
     console.log(user);
 }
 
@@ -13,4 +14,4 @@ main()
     })
     .finally(async () => {
         await prisma.$disconnect()
-    })
+    });
